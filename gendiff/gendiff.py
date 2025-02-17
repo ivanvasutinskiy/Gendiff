d2d775946bab5df46d2c6438b9665d1496399f1b
Ivan <ivan0vasutinskiy@gmail.com>
2025-02-17 21:37:37 +0300

    update pyproject.toml

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 def description_of_thegender_spread():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
@@ -7,3 +9,15 @@ def description_of_thegender_spread():
     parser.add_argument('second_file', type=str)
     my_namespace = parser.parse_args()
     print(my_namespace.FORMAT)
+
+def first_file():
+    with open(os.path.abspath('gendiff/file1.json')) as json_file:
+        data = json.load(json_file)
+        data1 = json.dumps(data, sort_keys=True, indent=4)
+        print(data1)
+
+def second_file():
+    with open(os.path.abspath('gendiff/file2.json')) as json_file:
+        data = json.load(json_file)
+        data2 = json.dumps(data, sort_keys=True, indent=4)
+        print(data2)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,28 +1,23 @@
 import os
 import json
-from gendiff.gendiff import description_of_thegender_spread
+from gendiff.gendiff import description_of_thegender_spread, first_file, second_file
 
 def main():
     description_of_thegender_spread()
 
-
 if __name__ == "__main__":
     main()
 
-def first_file():
-    with open(os.path.abspath('gendiff/file1.json')) as json_file:
-        data = json.load(json_file)
-        data1 = json.dumps(data, sort_keys=True, indent=4)
-        print(data1)
 
-if __name__ == "__firs_file__":
-    firs_file()
+def file_path1():
+    first_file()
 
-def second_file():
-    with open(os.path.abspath('gendiff/file2.json')) as json_file:
-        data = json.load(json_file)
-        data2 = json.dumps(data, sort_keys=True, indent=4)
-        print(data2)
-    
-if __name__ == "__second_file__":
+if __name__ == "__file_path1__":
+    file_path1()
+
+
+def file_path2():
     second_file()
+
+if __name__ == "__file_path2__":
+    file_path2()
