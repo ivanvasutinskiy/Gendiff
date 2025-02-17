e02e75f831a14ca11567009dcfb7d90a121772d1
Ivan <ivan0vasutinskiy@gmail.com>
2025-02-17 10:40:07 +0300

    update pyproject.toml, update gendiff.py and touch file1.json, file2.json

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,5 @@
+import os
+import json
 from gendiff.gendiff import description_of_thegender_spread
 
 def main():
@@ -6,3 +8,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def get_gendiff():
+    with open(os.path.abspath('gendiff/file1.json')) as json_file:
+        data = json.load(json_file)
+        data1 = json.dumps(data, sort_keys=True, indent=4)
+        print(data1)
+    with open(os.path.abspath('gendiff/file2.json')) as json_file:
+        data = json.load(json_file)
+        data2 = json.dumps(data, sort_keys=True, indent=4)
+        print(data2)
+    
+if __name__ == "__get_gendiff__":
+    get_gendiff()
