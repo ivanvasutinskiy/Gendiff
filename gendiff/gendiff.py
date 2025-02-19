279c7b23ffe301ddd91d543bf88aa8e0db285ee4
Ivan <ivan0vasutinskiy@gmail.com>
2025-02-19 20:27:41 +0300

    update generate_diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -12,7 +12,6 @@ def description_of_thegender_spread():
     dict1 = json.loads(first_f(args.first_file))
     dict2 = json.loads(second_f(args.second_file))
     diff =  (compare_dictionaries(dict1, dict2))
-    diff = json.dumps(diff, indent=2)
     print(diff)
 
 def first_f(file1):
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,17 +1,21 @@
+from re import sub
+import json
+
 def compare_dictionaries(dict1, dict2):
     differences = {}
 
     for key in dict1:
         if key not in dict2:
-            differences['-'+key] = dict1[key]
+            differences[f'- {key}'] = dict1[key]
         elif dict1[key] != dict2[key]:
-            differences['-'+key] = dict1[key]
-            differences['+'+key] = dict2[key]
+            differences[f'- {key}'] = dict1[key]
+            differences[f'+ {key}'] = dict2[key]
         else:
-            differences[key] = dict1[key]
+            differences[f'  {key}'] = dict1[key]
 
     for key in dict2:
         if key not in dict1:
-            differences['+'+key] =  dict2[key]
-
-    return differences
+            differences[f'+ {key}'] =  dict2[key]
+    differences = json.dumps(differences, indent=4)
+    result = sub('["'',]', '', differences)
+    return result
