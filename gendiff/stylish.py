cc4714ba9c5541dd989d79f05d01b394c1f4e506
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-13 17:41:39 +0300

    update stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -9,20 +9,20 @@ def transform_changes(changes):
             return f"{indent}- {item['name']}: {to_str(item['old_value'])}\n"
         elif item['action'] == 'modified':
             return f"{indent}- {item['name']}: {to_str(item['old_value'])}\n" + \
-            f"..{indent}+ {item['name']}: {to_str(item['new_value'])}\n"
+            f"{indent}{'..' * (depth - 1)}+ {item['name']}: {to_str(item['new_value'])}\n"
         elif item['action'] == 'unchanged':
             return f"{indent}  {item['name']}: {to_str(item['value'])}\n"
         elif item['action'] == 'nested':
             nested_result = f"{indent}  {item['name']}: {{\n"
             for child in item['children']:
                 nested_result += f"{indent}{process_node(child, depth + 1)}"
-            nested_result += f"{indent}  }}\n"
+            nested_result += f"{indent}}}\n"
             return nested_result
 
     result = ['{']
     for item in changes:
         result.append(process_node(item, 1))
-    return '\n'.join(result) + '\n}'
+    return '\n'.join(result) + '\n-}'
 
 
 def to_str(value):
