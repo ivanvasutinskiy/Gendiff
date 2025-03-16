700847dd02e1a8a2f629bb851cb5dd45c741ce41
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-16 21:19:37 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -18,15 +18,18 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
 
 
 def load_file(file_path):
-    with open(os.path.abspath(f'tests/fixtures/{file_path}')) as file:
-        extension = file_path.split('.')[-1]
-        if extension == 'json':
-            data_object = json.load(file)
-            data = json.dumps(data_object, sort_keys=True, indent=4)
-            return parse(data, extension)
-        if extension == 'yaml' or 'yml':
-            data_object = yaml.load(file, Loader=SafeLoader)
-            data = yaml.dump(data_object, indent=4)
-            return parse(data, extension)
+    for root, dirs, files in os.walk('/'):
+        if file_path in files:
+            file_path = os.path.join(root, file_path)
+            with open(os.path.abspath(file_path)) as file:
+                extension = file_path.split('.')[-1]
+                if extension == 'json':
+                    data_object = json.load(file)
+                    data = json.dumps(data_object, sort_keys=True, indent=4)
+                    return parse(data, extension)
+                if extension == 'yaml' or 'yml':
+                    data_object = yaml.load(file, Loader=SafeLoader)
+                    data = yaml.dump(data_object, indent=4)
+                    return parse(data, extension)
 
 
