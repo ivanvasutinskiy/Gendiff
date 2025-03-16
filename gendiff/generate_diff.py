ffeaa41a2b988b578abb7bd89818b87b70b97cd9
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-16 21:35:59 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -21,15 +21,18 @@ def load_file(file_path):
     for root, dirs, files in os.walk('/'):
         if file_path in files:
             file_path = os.path.join(root, file_path)
-            with open(os.path.abspath(file_path)) as file:
-                extension = file_path.split('.')[-1]
-                if extension == 'json':
-                    data_object = json.load(file)
-                    data = json.dumps(data_object, sort_keys=True, indent=4)
-                    return parse(data, extension)
-                if extension == 'yaml' or 'yml':
-                    data_object = yaml.load(file, Loader=SafeLoader)
-                    data = yaml.dump(data_object, indent=4)
-                    return parse(data, extension)
-
+            try: 
+                with open(os.path.abspath(file_path)) as file:
+                    extension = file_path.split('.')[-1]
+                    if extension == 'json':
+                        data_object = json.load(file)
+                        data = json.dumps(data_object, sort_keys=True, indent=4)
+                        return parse(data, extension) 
+                    if extension == 'yaml' or 'yml':
+                        data_object = yaml.load(file, Loader=SafeLoader)
+                        data = yaml.dump(data_object, indent=4)
+                        return parse(data, extension)
+            except FileNotFoundError:
+                print(f"Файл не найден: {file_path}")
+            return None
 
