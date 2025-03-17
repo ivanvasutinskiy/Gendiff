0b3c75e60d7aa61afd5ad0e08cf2f28e55d1fb1b
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-17 09:52:28 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -16,23 +16,25 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     dict_diff = get_difference(data1, data2)
     return select_formatter(dict_diff, format_name)
 
-
 def load_file(file_path):
     for root, dirs, files in os.walk('/'):
         if file_path in files:
             file_path = os.path.join(root, file_path)
             try: 
-                with open(os.path.abspath(file_path)) as file:
+                with open(os.path.abspath(file_path), 'r') as file:
                     extension = file_path.split('.')[-1]
                     if extension == 'json':
                         data_object = json.load(file)
                         data = json.dumps(data_object, sort_keys=True, indent=4)
                         return parse(data, extension) 
-                    if extension == 'yaml' or 'yml':
+                    elif extension in ('yaml', 'yml'):
                         data_object = yaml.load(file, Loader=SafeLoader)
                         data = yaml.dump(data_object, indent=4)
                         return parse(data, extension)
             except FileNotFoundError:
                 print(f"Файл не найден: {file_path}")
-            return None
+            except Exception as e:
+                print(f"Произошла ошибка при обработке файла: {str(e)}")
+    return None
+
 
