d01d56eb540ad9352f1f206b969d7cfef5d65b7e
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-17 11:03:03 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -20,21 +20,15 @@ def load_file(file_path):
     for root, dirs, files in os.walk('/'):
         if file_path in files:
             file_path = os.path.join(root, file_path)
-            print(file_path)
-            try: 
-                with open(os.path.abspath(file_path), 'r') as file:
-                    extension = file_path.split('.')[-1]
-                    if extension == 'json':
-                        data_object = json.load(file)
-                        data = json.dumps(data_object, sort_keys=True, indent=4)
-                        return parse(data, extension) 
-                    elif extension in ('yaml', 'yml'):
-                        data_object = yaml.load(file, Loader=SafeLoader)
-                        data = yaml.dump(data_object, indent=4)
-                        return parse(data, extension)
-            except FileNotFoundError:
-                print(f"Файл не найден: {file_path}")
-            except Exception as e:
-                print(f"Произошла ошибка при обработке файла: {str(e)}")
+            with open(os.path.abspath(file_path), 'r') as file:
+                extension = file_path.split('.')[-1]
+                if extension == 'json':
+                    data_object = json.load(file)
+                    data = json.dumps(data_object, sort_keys=True, indent=4)
+                    return parse(data, extension) 
+                elif extension in ('yaml', 'yml'):
+                    data_object = yaml.load(file, Loader=SafeLoader)
+                    data = yaml.dump(data_object, indent=4)
+                    return parse(data, extension)
 
 
