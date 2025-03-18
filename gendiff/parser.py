885b4d1aa783c9aadf4fd23376efc3d795305b1f
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-18 17:14:11 +0300

    update parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -15,8 +15,5 @@ def parse(data, format_name):
             return json.load(data)
         case 'yml' | 'yaml':
             return yaml.safe_load(data)
-        case _:
-            raise ValueError("Unsupported data format")
-    
     
     
