71cd0c76081f629691f5454f7e1e25eb5d7c07b6
Ivan <ivan0vasutinskiy@gmail.com>
2025-03-21 17:46:55 +0300

    update tests

--- tests/test_gendiff_plain.py
+++ tests/test_gendiff_plain.py
@@ -5,83 +5,18 @@ from gendiff.generate_diff import generate_diff
 
 @pytest.fixture
 def file_one():
-    return {
-  "common": {
-    "setting1": "Value 1",
-    "setting2": 200,
-    "setting3": True,
-    "setting6": {
-      "key": "value",
-      "doge": {
-        "wow": ""
-      }
-    }
-  },
-  "group1": {
-    "baz": "bas",
-    "foo": "bar",
-    "nest": {
-      "key": "value"
-    }
-  },
-  "group2": {
-    "abc": 12345,
-    "deep": {
-      "id": 45
-    }
-  }
-}
+    return open('./tests/fixtures/file1_for_tests.txt')
 
 
 @pytest.fixture
 def file_two():
-    return {
-  "common": {
-    "follow": False,
-    "setting1": "Value 1",
-    "setting3": None,
-    "setting4": "blah blah",
-    "setting5": {
-      "key5": "value5"
-    },
-    "setting6": {
-      "key": "value",
-      "ops": "vops",
-      "doge": {
-        "wow": "so much"
-      }
-    }
-  },
-  "group1": {
-    "foo": "bar",
-    "baz": "bars",
-    "nest": "str"
-  },
-  "group3": {
-    "deep": {
-      "id": {
-        "number": 45
-      }
-    },
-    "fee": 100500
-  }
-}
+    return open('./tests/fixtures/file2_for_tests.txt')
 
 
 @pytest.fixture
 def difference():
-    return """Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]
-"""
+    with open('./tests/fixtures/result_plain.txt') as file:
+        return file.read()
 
 
 def test_generate_diff_json(difference):
--- tests/test_gendiff_stylish.py
+++ tests/test_gendiff_stylish.py
@@ -5,115 +5,18 @@ from gendiff.generate_diff import generate_diff
 
 @pytest.fixture
 def file_one():
-    return {
-  "common": {
-    "setting1": "Value 1",
-    "setting2": 200,
-    "setting3": True,
-    "setting6": {
-      "key": "value",
-      "doge": {
-        "wow": ""
-      }
-    }
-  },
-  "group1": {
-    "baz": "bas",
-    "foo": "bar",
-    "nest": {
-      "key": "value"
-    }
-  },
-  "group2": {
-    "abc": 12345,
-    "deep": {
-      "id": 45
-    }
-  }
-}
+    return open('./tests/fixtures/file1_for_tests.txt')
 
 
 @pytest.fixture
 def file_two():
-    return {
-  "common": {
-    "follow": False,
-    "setting1": "Value 1",
-    "setting3": None,
-    "setting4": "blah blah",
-    "setting5": {
-      "key5": "value5"
-    },
-    "setting6": {
-      "key": "value",
-      "ops": "vops",
-      "doge": {
-        "wow": "so much"
-      }
-    }
-  },
-  "group1": {
-    "foo": "bar",
-    "baz": "bars",
-    "nest": "str"
-  },
-  "group3": {
-    "deep": {
-      "id": {
-        "number": 45
-      }
-    },
-    "fee": 100500
-  }
-}
+    return open('./tests/fixtures/file2_for_tests.txt')
 
 
 @pytest.fixture
 def difference():
-    return """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}"""
+    with open('./tests/fixtures/result_stylish.txt') as file:
+        return file.read()
 
 
 def test_generate_diff_json(difference):
